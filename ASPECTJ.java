1fe177bb997d5f8f87dda240f0b8cf0877ac5881
060210 <98629073+060210@users.noreply.github.com>
2022-02-19 08:43:58 +0700

    Update ASPECTJ.java

--- ASPECTJ.java
+++ ASPECTJ.java
@@ -8,14 +8,3 @@ public class HelloWorld {
         System.out.println(name + ", " + message);
     }
 }
-
-// MannersAspect.java
-public aspect MannersAspect {
-    pointcut callSayMessage() : call(public static void HelloWorld.say*(..));
-    before() : callSayMessage() {
-        System.out.println("Good day!");
-    }
-    after() : callSayMessage() {
-        System.out.println("Thank you!");
-    }
-}
